fc047c3ffc0d7b50f7895e60cfc4931146975894
kimsungho <tigertjdgh@gmail.com>
2023-06-05 20:07:27 +0900

    Initial commit

---
+++ src/main/java/com/example/dividend/scheduler/ScraperScheduler.java
@@ -0,0 +1,72 @@
+package com.example.dividend.scheduler;
+
+import com.example.dividend.model.Company;
+import com.example.dividend.model.ScrapedResult;
+import com.example.dividend.model.constants.CacheKey;
+import com.example.dividend.persist.CompanyRepository;
+import com.example.dividend.persist.DividendRepository;
+import com.example.dividend.persist.entity.CompanyEntity;
+import com.example.dividend.persist.entity.DividendEntity;
+import com.example.dividend.scraper.Scraper;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@AllArgsConstructor
+@Slf4j
+@EnableCaching
+public class ScraperScheduler {
+
+    //    @Scheduled(cron = "0/5 * * * * *")
+//    public void test() {
+//        System.out.println("now -> " + System.currentTimeMillis());
+//    }
+    private final CompanyRepository companyRepository;
+    private final DividendRepository dividendRepository;
+
+    private final Scraper yahooFinanceScraper;
+
+    // 일정 주기마다 수행
+    @Scheduled(cron = "${scheduler.scrap.yahoo}")
+    @CacheEvict(value = CacheKey.KEY_FINANCE, allEntries = true)
+    public void yahooFinanceScheduling() {
+        // 저장된 회사 목록을 조회
+        log.info("scraping scheduler is started");
+        List<CompanyEntity> companies = this.companyRepository.findAll();
+
+        // 회사마다 배당금 정보를 새로 스크래핑
+        for (var company : companies) {
+            log.info("scraping scheduler is started -> " + company.getName());
+            ScrapedResult scrapedResult = this.yahooFinanceScraper.scrap(
+                    new Company(company.getTicker(), company.getName()));
+
+            // 스크래핑한 배당금 정보 중 데이터베이스에 없는 값은 저장
+            scrapedResult.getDividendEntities().stream()
+                    // 디비든 모델을 디비든 엔티티로 매핑
+                    .map(e -> new DividendEntity(company.getId(), e))
+                    // 엘리먼트를 하나씩 디비든 레파지토리에 삽입
+                    .forEach(e -> {
+                        boolean exists = this.dividendRepository.existsByCompanyIdAndDate(e.getCompanyId(), e.getDate());
+                        if (!exists) {
+                            this.dividendRepository.save(e);
+                            log.info("insert new dividend -> " + e.toString());
+                        }
+                    });
+            // 연속적으로 스크래핑 대상 사이트 서버에 요청을 날리지 않도록 일시정지
+            try {
+                Thread.sleep(3000); // 3 seconds
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+        }
+
+
+    }
+}
---
+++ src/main/java/com/example/dividend/scraper/YahooFinanceScraper.java
@@ -0,0 +1,93 @@
+package com.example.dividend.scraper;
+
+import com.example.dividend.model.Company;
+import com.example.dividend.model.Dividend;
+import com.example.dividend.model.ScrapedResult;
+import com.example.dividend.model.constants.Month;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class YahooFinanceScraper implements Scraper{
+    private static final String STATISTICS_URL = "https://finance.yahoo.com/quote/COKE/history?period1=99100800&period2=1684800000&interval=1mo";
+    private static final String SUMMARY_URL = "https://finance.yahoo.com/quote/%s?p=%s";
+    private static final long START_TIME = 86400; // 60 * 60 * 24
+
+    @Override
+    public ScrapedResult scrap(Company company) {
+        var scrapedResult = new ScrapedResult();
+        scrapedResult.setCompany(company);
+
+        try {
+            long start = 0; // 시작 시간
+            long end = System.currentTimeMillis() / 1000; // 끝 시간
+
+            String url = String.format(STATISTICS_URL, company.getTicker(), START_TIME, end);
+            // 야후 finace url을 넣어준다.
+            Connection connection = Jsoup.connect(url);
+            Document document = connection.get();
+
+            Elements parsingDivs = document.getElementsByAttributeValue("data-test", "historical-prices");
+            Element tableEle = parsingDivs.get(0); // table 전체
+
+            Element tbody = tableEle.children().get(1);
+
+            List<Dividend> dividends = new ArrayList<>();
+            for (Element e : tbody.children()) {
+                String txt = e.text();
+                if (!txt.endsWith("Dividend")) {
+                    continue;
+                }
+
+                String[] splits = txt.split(" "); // 공백으로 데이터 스플릿
+                int month = Month.strToNumber(splits[0]);
+                int day = Integer.valueOf(splits[1].replace(",", ""));
+                int year = Integer.valueOf(splits[2]);
+                String dividend = splits[3];
+
+                if (month < 0) {
+                    throw new RuntimeException("Unexpected Month enum value -> " + splits[0]);
+                }
+
+                dividends.add(new Dividend(LocalDateTime.of(year, month, day, 0, 0), dividend));
+
+//                System.out.println(year + "/" + month + "/" + day + " -> " + dividend);
+            }
+
+            scrapedResult.setDividendEntities(dividends);
+
+        } catch (IOException e) {
+            // TODO
+            e.printStackTrace();
+        }
+        return scrapedResult;
+    }
+
+    @Override
+    public Company scrapCompanyByTicker(String ticker) {
+        String url = String.format(SUMMARY_URL, ticker, ticker);
+
+        try {
+            Document document = Jsoup.connect(url).get();
+            Element titleEle = document.getElementsByTag("h1").get(0);
+            String title = titleEle.text().split(" - ")[1].trim();
+            // abc - def
+            return new Company(ticker, title);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+}
